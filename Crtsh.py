473fe9b8ddf1983ee2135caa82a290f2697f54c0
DClaeyman <damien.claeyman.20160288@efrei.net>
2022-02-10 15:32:49 +0200

    Setter Getter - v1.1

--- Crtsh.py
+++ Crtsh.py
@@ -17,7 +17,7 @@ class Crtsh:
             self.__output = f'{domain}_subdomains.csv'
             self.__output_log = f'{domain}_subdomains.log'
 
-    def scrapper(self):
+    def __scrapper(self):
         print(f'Scrapping: {self.CRTSH_URL}?CN=%25.devoteam.com')
         response = requests.get(f'{self.CRTSH_URL}?CN=%25.devoteam.com')
         tree = html.fromstring(response.content)
@@ -27,7 +27,7 @@ class Crtsh:
         return sorted(set(sub_domain))
 
     @staticmethod
-    def save(data, output):
+    def __save(data, output):
         try:
             file = open(output, 'a', newline='')
             with file:
@@ -37,7 +37,7 @@ class Crtsh:
         except IOError:
             print(f'Impossible to save data')
 
-    def load(self):
+    def __load(self):
         try:
             file = open(self.__output, 'r')
             data = []
@@ -48,13 +48,13 @@ class Crtsh:
         except IOError:
             print(f'File: {self.__output} doesn\'t exist')
 
-    def log(self, result):
+    def __log(self, result):
         date = f'\n[{datetime.now()}]'
-        self.save([date], self.__output_log)
-        self.save(result, self.__output_log)
+        self.__save([date], self.__output_log)
+        self.__save(result, self.__output_log)
 
     @staticmethod
-    def comparator(old_data, new_data):
+    def __comparator(old_data, new_data):
 
         new_subdomains = []
 
@@ -69,21 +69,38 @@ class Crtsh:
         return new_subdomains
 
     def exec(self):
-        new_data = self.scrapper()
-        old_data = self.load()
+        new_data = self.__scrapper()
+        old_data = self.__load()
 
         if old_data:
-            new_subdomains = self.comparator(old_data, new_data)
+            new_subdomains = self.__comparator(old_data, new_data)
         else:
             new_subdomains = new_data
 
         if new_subdomains:
-            self.log(new_subdomains)
-            self.save(new_subdomains, self.__output)
+            self.__log(new_subdomains)
+            self.__save(new_subdomains, self.__output)
 
             print(f'New subdomains found:')
             for new_subdomain in new_subdomains:
                 print(new_subdomain)
         else:
-            self.log(['No new subdomains'])
+            self.__log(['No new subdomains'])
             print('No new subdomains')
+
+    @property
+    def domain(self):
+        return self.__domain
+
+    @domain.setter
+    def domain(self, value):
+        self.__domain = value
+
+    @property
+    def output(self):
+        return self.__output
+
+    @output.setter
+    def output(self, value):
+        self.__output = f'{value}/{self.__domain}_subdomains.csv'
+        self.__output_log = f'{value}/{self.__domain}_subdomains.log'
